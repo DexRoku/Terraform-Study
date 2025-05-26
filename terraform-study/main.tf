802159e0613d2a300004fa1190e34450213603fd
Rohith Raju <krishna.raju93@gmail.com>
2025-05-26 21:21:37 +0200

    Adding new subnet and vpc

--- terraform-study/main.tf
+++ terraform-study/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "************"
-    secret_key = "************"
+    access_key = ""
+    secret_key = ""
 }
 
 
@@ -22,6 +22,13 @@ resource "aws_vpc" "terraform-study-vpc" {
     }
 }
 
+resource "aws_vpc" "terraform-study-vpc-2" {
+    cidr_block = "10.0.1.0/16"
+
+    tags = {
+        Name = "dev-terraform-study-vpc-2"
+    }
+}
 
 resource "aws_subnet" "subnet-1" {
     vpc_id = aws_vpc.terraform-study-vpc.id
@@ -29,5 +36,14 @@ resource "aws_subnet" "subnet-1" {
 
     tags = {
         Name = "prod-subnet-1"
+    }
+}
+
+resource "aws_subnet" "subnet-2" {
+    vpc_id = aws_vpc.terraform-study-vpc-2.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+        Name = "prod-subnet-2"
     }
 }
